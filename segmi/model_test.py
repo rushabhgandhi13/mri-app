dd10a47ab250d515c999c27ed41f5a68d09b5bc6
pathikg <pathikghugare13@gmail.com>
2022-04-29 02:03:12 +0530

    updated utils with docstring

--- segmi/model_test.py
+++ segmi/model_test.py
@@ -6,10 +6,10 @@ model = load_model()
 checkpoint_filepath = './model_checkpoint/'
 model.load_weights(checkpoint_filepath)
 
-input = load_volume("image_270.npy")
+input = load_volume("image_118.npy")
 
 # outputs a numpy array of shape (128, 128, 128,4)
 output = predict(model, input)
 # outputs 64th slice of the output of shape (128, 128)
-output_slice = rgb_output(output, "test.png", slice_num=64)
+output_slice = argmax_output(output, slice_num=64)
 plot_save_slice(input, output_slice)
