29ac78ad6ba70ee3c50b08f2b7ec8ac52d2a6a24
pathikg <pathikghugare13@gmail.com>
2022-04-29 01:50:23 +0530

    model tests

---
+++ segmi/model_test.py
@@ -0,0 +1,15 @@
+from utils import *
+set_seed(42)
+
+model = load_model()
+
+checkpoint_filepath = './model_checkpoint/'
+model.load_weights(checkpoint_filepath)
+
+input = load_volume("image_270.npy")
+
+# outputs a numpy array of shape (128, 128, 128,4)
+output = predict(model, input)
+# outputs 64th slice of the output of shape (128, 128)
+output_slice = rgb_output(output, "test.png", slice_num=64)
+plot_save_slice(input, output_slice)
